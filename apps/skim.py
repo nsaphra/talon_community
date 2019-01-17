fcf6742e6a6fed5351a0a94ee0ebc9743df43e70
Naomi Saphra <nsaphra@gmail.com>
2019-01-17 14:20:59 +0000

    Add tools for annotating PDFs

--- apps/skim.py
+++ apps/skim.py
@@ -1,13 +1,11 @@
 from talon.voice import Key, press, Str, Context
 # Commands for annotating pdfs.
 
-ctx = Context('skim', func=lambda app, window:  'com.apple.Preview' in app.bundle or  'net.sourceforge.skim-app.skim' in app.bundle)
+ctx = Context('skim', bundle='net.sourceforge.skim-app.skim')
 
 keymap = {
     'highlight': Key('cmd-ctrl-h'),
     'anchor': Key('cmd-alt-n'),
-    'note': Key('cmd-ctrl-n'),
-
 }
 
 ctx.keymap(keymap)
